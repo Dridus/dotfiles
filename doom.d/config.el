06f8abd7de841f51aaf18252979549c91f22c383
Ross MacLeod <rmm+github@z.odi.ac>
2020-12-07 12:26:56 -0500

    another turn on the editor merry go round

---
+++ doom.d/config.el
@@ -0,0 +1,8 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Ross MacLeod"
+      user-mail-address "rmm+github@z.odi.ac")
+(setq doom-font "Fira Code Retina-12")
+(setq doom-theme 'doom-molokai)
+(setq org-directory "~/org/")
+(setq display-line-numbers-type 'relative)
