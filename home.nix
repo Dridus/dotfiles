99e47d7b65e8e4b3a7338e0c565737c61b589a39
Ross MacLeod <ross@vitalbio.com>
2022-05-27 18:59:39 -0400

    gitFull -> git, remove helix, add nushell

--- home.nix
+++ home.nix
@@ -17,39 +17,6 @@ let
     fetchSubmodules = true;
   };
 
-  helix = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "helix";
-    version = "22.03";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "helix-editor";
-      repo = pname;
-      rev = "${version}";
-      fetchSubmodules = true;
-      sha256 = "sha256-anUYKgr61QQmdraSYpvFY/2sG5hkN3a2MwplNZMEyfI=";
-    };
-
-    cargoSha256 = "sha256-zJQ+KvO+6iUIb0eJ+LnMbitxaqTxfqgu7XXj3j0GiX4=";
-
-    nativeBuildInputs = [ pkgs.makeWrapper ];
-
-    postInstall = ''
-      mkdir -p $out/lib
-      cp -r runtime $out/lib
-    '';
-    postFixup = ''
-      wrapProgram $out/bin/hx --set HELIX_RUNTIME $out/lib/runtime
-    '';
-
-    meta = with lib; {
-      description = "A post-modern modal text editor";
-      homepage = "https://helix-editor.com";
-      license = licenses.mpl20;
-      mainProgram = "hx";
-      maintainers = with maintainers; [ yusdacra ];
-    };
-  };
-
 in
 
 {
@@ -64,6 +31,7 @@ in
     };
 
     packages = with pkgs; [
+      nushell
       zsh-prezto
       neovim
       file
@@ -119,7 +87,7 @@ in
     home-manager.enable = true;
 
     git = {
-      package = pkgs.gitAndTools.gitFull;
+      package = pkgs.git;
       enable = true;
       userName = "Ross MacLeod";
       extraConfig = {
