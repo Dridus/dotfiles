79145f4412b94efa2d7a889299118fe47426f2cd
Ross MacLeod <rmm+github@z.odi.ac>
2022-07-16 18:53:07 -0400

    whitespace

--- nvim/lua/rmm/keys/buffer.lua
+++ nvim/lua/rmm/keys/buffer.lua
@@ -3,16 +3,19 @@ vim.keymap.set(
   ":bp<cr>",
   { desc = "Previous buffer" }
 )
+
 vim.keymap.set(
   "n", "<Leader>bn",
   ":bn<cr>",
   { desc = "Next buffer" }
 )
+
 vim.keymap.set(
   "n", "<Leader>bd",
   ":Bd<cr>",
   { desc = "Delete buffer without closing window" }
 )
+
 vim.keymap.set(
   "n", "<Leader>bD",
   ":1,$bd<cr>",
