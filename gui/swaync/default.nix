b2d54c6bf2fabe1434c59c33d6b5c761017698d1
Ross MacLeod <rmm+github@z.odi.ac>
2024-03-30 21:33:20 -0400

    gui/swaync: fix assertion message copy-pasta

--- gui/swaync/default.nix
+++ gui/swaync/default.nix
@@ -10,7 +10,7 @@
         cfg = config.wayland.windowManager.hyprland;
       in
         cfg.enable && cfg.systemd.enable;
-      message = "gui/ironbar expects hyprland";
+      message = "gui/swaync expects hyprland";
     }
   ];
 
