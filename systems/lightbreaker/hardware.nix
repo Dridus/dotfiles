8e0454bb75ecd5f0808209068584e2423955a04e
Ross MacLeod <ross@vitalbio.com>
2024-05-18 19:09:00 -0400

    lightbreaker: enable parallels guest with some light patching

--- systems/lightbreaker/hardware.nix
+++ systems/lightbreaker/hardware.nix
@@ -1,4 +1,10 @@
-{ lib, modulesPath, ... }:
+{
+  config,
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
 
 {
   imports = [ "${modulesPath}/virtualisation/parallels-guest.nix" ];
@@ -26,7 +32,19 @@
     fsType = "ext4";
   };
 
-  # hardware.parallels.enable = true;
+  hardware.parallels = {
+    enable = true;
+    package = config.boot.kernelPackages.prl-tools.overrideAttrs (
+      final: prev: {
+        postInstall = ''
+          ${pkgs.bbe}/bin/bbe -e "s:/sbin/modprobe prl_tg\x00:modprobe prl_tg\x00\x00\x00\x00\x00\x00\x00:" -o $out/bin/.prltoolsd-wrapped.tmp $out/bin/.prltoolsd-wrapped
+          mv $out/bin/.prltoolsd-wrapped{.tmp,}
+          chmod 755 $out/bin/.prltoolsd-wrapped
+          sed -i -e '/^exec/ i export PATH=$PATH:${pkgs.kmod}/bin' $out/bin/prltoolsd
+        '';
+      }
+    );
+  };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/562a20d5-82f8-485c-8995-b5b4d55116cb"; } ];
 
