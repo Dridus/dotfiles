5f7dffeb2199c6c371ba121dbe2f4568411bfc75
Ross MacLeod <ross@vitalbio.com>
2025-03-06 20:25:44 -0500

    cli/git: branch.autoSetupMerge=simple

--- cli/git/default.nix
+++ cli/git/default.nix
@@ -14,22 +14,23 @@
     package = pkgs.git;
     enable = true;
     extraConfig = {
+      add.interactive.useBuiltin = false;
+      branch.autoSetupMerge = "simple";
       core.editor = "$EDITOR";
       core.pager = "${pkgs.delta}/bin/delta";
-      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
-      add.interactive.useBuiltin = false;
-      include.path = "${pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/dandavison/delta/4c879ac1afca68a30c9a100bea2965b858eb1853/themes.gitconfig";
-        sha256 = "d+g4xcnMicguU9LhsdeZUrB8r6PKT0E79iAo7lLRuxI=";
-      }}";
       delta = {
         features = "chameleon";
         side-by-side = false;
         navigate = true;
         light = false;
       };
-      merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
+      include.path = "${pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/dandavison/delta/4c879ac1afca68a30c9a100bea2965b858eb1853/themes.gitconfig";
+        sha256 = "d+g4xcnMicguU9LhsdeZUrB8r6PKT0E79iAo7lLRuxI=";
+      }}";
+      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
+      merge.conflictstyle = "diff3";
     };
   };
 }
